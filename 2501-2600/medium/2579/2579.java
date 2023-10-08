fd5330bf525a3e831548799f9648d6680a83b86f
Avi Gupta <avg8377@gmail.com>
2023-10-08 13:29:04 +0530

    one liner solution

--- 2501-2600/medium/2579/2579.java
+++ 2501-2600/medium/2579/2579.java
@@ -1,8 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        if(n == 1) {
-            return 1;
-        }
-        return (long)(n-1)*(2 + (n-2)*2) + 2*n-1;
+        return Math.abs((long)(n-1)*(2 + (n-2)*2) + 2*n-1);
     }
 }
