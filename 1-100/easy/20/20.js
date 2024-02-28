9686de9c36eb01e5b8649da4bdbd8ad69e7451df
Anjali Singh <50075812+anjiattri@users.noreply.github.com>
2024-02-28 15:17:24 +0530

    add 20

---
+++ 1-100/easy/20/20.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack=[]
+    for(let i=0;i<s.length;i++){
+        if(s[i]==='(' || s[i]==='{' || s[i]==='['){
+            stack.push(s[i])
+
+        }
+        else if(s[i]===')' || s[i]==='}' || s[i]===']'){
+            if(stack.length==0){
+                return false
+            }
+            const top=stack.pop()
+            if((s[i]===')' && top!=='(') || (s[i]==='}' && top!=='{') || (s[i]===']' && top!=='[') ){
+                return false
+            }
+        }
+    }
+    return stack.length==0
+};
